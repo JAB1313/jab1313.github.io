96520dc4b4e533af122a09102aff617b98dd7cee
JAB1313 <jbetancourt@gmail.com>
2016-12-11 17:06:00 -0500

    Added a movie search

---
+++ movie-search/scripts/app.js
@@ -0,0 +1,38 @@
+
+function searchIMDB(query) {
+  // http://www.omdbapi.com/?t=rocky&y=&plot=short&r=json
+  $.getJSON('http://www.omdbapi.com/', {
+    t: query,
+    plot: "short",
+    r: 'json'
+  }, function(omdbData) {
+    let results = omdbData;
+    if (omdbData.Response == "True"){
+      renderMovie(omdbData);
+    } else {
+      renderError();
+    }
+  });
+}
+
+function renderMovie(data) {
+  $('.result').show();
+  $('#poster').attr("src", data.Poster);
+  $('#title').html(data.Title);
+  $('#year').html(data.Year);
+  $('#actors').html(data.Actors);
+  $('#director').html(data.Director);
+  $('#imdbRating').html(data.imdbRating);
+}
+
+function renderError() {
+  $('.error').show();
+}
+
+
+$('#movie-search-form').on('submit', function(event) {
+  event.preventDefault(); // this prevents the page from reloading again -- this is one way that JavaScript lets you perform actions asynchronously
+  $('.result').hide();
+  searchIMDB(this.query.value);
+  this.query.value = '';
+});
